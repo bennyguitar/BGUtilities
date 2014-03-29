1d35303649ce7e9cfb988039365be34634954dd9
Ben Gordon <brgordon@ua.edu>
2014-03-29 11:20:11 -0500

    Added UIImage+BGImageUtilities

---
+++ Classes/UIImage+BGImageUtilities.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+BGImageUtilities.h
+//  BGUtilities
+//
+//  Created by Ben Gordon on 3/29/14.
+//  Copyright (c) 2014 Ben Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (BGImageUtilities)
+
+@end
---
+++ Classes/UIImage+BGImageUtilities.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+BGImageUtilities.m
+//  BGUtilities
+//
+//  Created by Ben Gordon on 3/29/14.
+//  Copyright (c) 2014 Ben Gordon. All rights reserved.
+//
+
+#import "UIImage+BGImageUtilities.h"
+
+@implementation UIImage (BGImageUtilities)
+
+@end
